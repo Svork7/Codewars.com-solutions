ac1b2b1098ff2518fb75d0cb5ad7cdc3edfa305f
Eugene Mikhailov <svork@ya.ru>
2023-01-19 12:28:19 +0300

    codewars.com

---
+++ Codewars.com 6 kyu solutions/Break camelCase.js
@@ -0,0 +1,9 @@
+/*DESCRIPTION:
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""*/
+
+let solution = (string) => string.replace(/([A-Z])/g, ' $1')
---
+++ Codewars.com 8 kyu solutions/a == a.js
@@ -0,0 +1,8 @@
+/*DESCRIPTION:
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a*/
+
+const a = []
